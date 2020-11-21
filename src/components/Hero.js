f43ffd83619fb306a66e3f10d7b2dde35a21af8e
212562268-Cherian,Shejoy <212562268@ge.com>
2020-11-21 20:15:03 +0530

    image fix

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,43 +6,43 @@ import SocialLinks from "../constants/socialLinks"
 import { FaDivide } from "react-icons/fa"
 // ...GatsbyImageSharpFluid
 
-// const query = graphql`
-//   {
-//     file(relativePath: { eq: "hero-img.png" }) {
-//       id
-//       childImageSharp {
-//         fluid {
-//           ...GatsbyImageSharpFluid
-//         }
-//       }
-//     }
-//   }
-// `
+const query = graphql`
+  {
+    file(relativePath: { eq: "projects-1.jpg" }) {
+      id
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
 
 const Hero = () => {
-  // const {
-  //   file: {
-  //     childImageSharp: { fluid },
-  //   },
-  // } = useStaticQuery(query)
-  // return (
-  //   <header className="hero">
-  //     <div className="section-center hero-center">
-  //       <article className="hero-info">
-  //         <div>
-  //           <div className="underline"></div>
-  //           <h1>I'am Superman</h1>
-  //           <h3>Web and CMS enthusiast</h3>
-  //           <Link to="contact" className="btn">
-  //             Contact Me
-  //           </Link>
-  //           <SocialLinks />
-  //         </div>
-  //       </article>
-  //       <Image fluid={fluid} className="hero-img" />
-  //     </div>
-  //   </header>
-  // )
+  const {
+    file: {
+      childImageSharp: { fluid },
+    },
+  } = useStaticQuery(query)
+  return (
+    <header className="hero">
+      <div className="section-center hero-center">
+        <article className="hero-info">
+          <div>
+            <div className="underline"></div>
+            <h1>I'am Superman</h1>
+            <h3>Web and CMS enthusiast</h3>
+            <Link to="contact" className="btn">
+              Contact Me
+            </Link>
+            <SocialLinks />
+          </div>
+        </article>
+        <Image fluid={fluid} className="hero-img" />
+      </div>
+    </header>
+  )
   return <div></div>
 }
 
