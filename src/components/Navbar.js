b8dfedf631d9318c42647f34ab1f6fc54e5c6788
212562268-Cherian,Shejoy <212562268@ge.com>
2020-11-21 09:13:44 +0530

    pagelinks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import logo from "../assets/logo.svg"
 import { FaAlignRight } from "react-icons/fa"
 import PageLinks from "../constants/links"
 const Navbar = () => {
-  return <h2>navbar component</h2>
+  return <PageLinks />
 }
 
 export default Navbar
