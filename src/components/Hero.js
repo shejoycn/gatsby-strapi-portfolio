84be3011bb01a08e2adb9f83e44a86f3904fbea9
212562268-Cherian,Shejoy <212562268@ge.com>
2020-11-21 14:49:25 +0530

    hero done

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,8 +5,43 @@ import { graphql, useStaticQuery } from "gatsby"
 import SocialLinks from "../constants/socialLinks"
 // ...GatsbyImageSharpFluid
 
+const query = graphql`
+  {
+    file(relativePath: { eq: "superman-img.png" }) {
+      id
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
+
 const Hero = () => {
-  return <h2>hero component</h2>
+  const {
+    file: {
+      childImageSharp: { fluid },
+    },
+  } = useStaticQuery(query)
+  return (
+    <header className="hero">
+      <div className="section-center hero-center">
+        <article className="hero-info">
+          <div>
+            <div className="underline"></div>
+            <h1>I'am Superman</h1>
+            <h3>Web and CMS enthusiast</h3>
+            <Link to="contact" className="btn">
+              Contact Me
+            </Link>
+            <SocialLinks />
+          </div>
+        </article>
+        <Image fluid={fluid} className="hero-img" />
+      </div>
+    </header>
+  )
 }
 
 export default Hero
